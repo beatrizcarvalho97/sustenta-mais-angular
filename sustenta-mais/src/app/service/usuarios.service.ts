5c921abae3ac854883c61961d62bc21f69ca1ae5
beatrizcarvalho97 <beatriz.carvalho.costa@hotmail.com>
2020-05-19 21:51:19 -0300

    Inclusão do método POST no Service.ts

--- sustenta-mais/src/app/service/usuarios.service.ts
+++ sustenta-mais/src/app/service/usuarios.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Usuarios } from '../model/Usuarios';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,10 @@ export class UsuariosService {
 
   getAllUsuarios(){
     return this.http.get('http://93.188.161.223:9000/user')
+  }
+
+  postUsuario(usuario: Usuarios){
+    return this.http.post('http://93.188.161.223:9000/user', usuario)
   }
 
 }
