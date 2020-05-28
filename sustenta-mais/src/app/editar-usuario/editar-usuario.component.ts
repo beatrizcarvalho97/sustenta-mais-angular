dd93ac2230e1e799b36a2fd3124f43cb35a96bee
beatrizcarvalho97 <beatriz.carvalho.costa@hotmail.com>
2020-05-27 23:06:55 -0300

    Inclusão do método EDITAR-USUÁRIOS

--- sustenta-mais/src/app/editar-usuario/editar-usuario.component.ts
+++ sustenta-mais/src/app/editar-usuario/editar-usuario.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuariosService } from '../service/usuarios.service';
 import { Usuarios } from '../model/Usuarios';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-editar-usuario',
@@ -7,11 +9,36 @@ import { Usuarios } from '../model/Usuarios';
   styleUrls: ['./editar-usuario.component.css']
 })
 export class EditarUsuarioComponent implements OnInit {
-confirmaSenha = "";
-usuario: Usuarios = new Usuarios
-  constructor() { }
+
+  usuario: Usuarios = new Usuarios
+
+  confirmaSenha = "";
+
+  constructor(private usuarioService: UsuariosService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+
+    let id = this.route.snapshot.params['id']
+    this.findById(id)
+  }
+
+  findById(id:number){
+    this.usuarioService.getByIdUsuario(id).subscribe((resp: Usuarios)=>{
+      this.usuario = resp
+    })
+  }
+
+  salvar(){
+    this.usuarioService.putUsuario(this.usuario).subscribe((resp: Usuarios)=>{
+      this.usuario = resp
+      this.router.navigate(['/lista-usuarios'])
+      location.assign('/lista-usuarios')
+    })
+  }
+
+  cancelar(){
+    this.router.navigate(['/lista-usuarios'])
   }
 
 }
+
